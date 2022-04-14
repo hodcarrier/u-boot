a546e9d9761bbcf5694efe7a79e17005eb36f743
Du Huanpeng <dhu@hodcarrier.org>
2022-04-15 01:31:44 +0800

    migrate peripheral id to CLK_APB
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

--- include/dt-bindings/clock/ls1c300-clk.h
+++ include/dt-bindings/clock/ls1c300-clk.h
@@ -6,43 +6,13 @@
 #ifndef __DT_BINDINGS_LS1C300_CLK_H__
 #define __DT_BINDINGS_LS1C300_CLK_H__
 
-/* Base clocks */
 #define CLK_XTAL	0
 #define CLK_PLL		1
 #define CLK_CPU		2
-#define CLK_CPU_THROT	7
-#define CLK_SDRAM	3
-
+#define CLK_APB 	3
 #define CLK_CAMERA	4
-#define CLK_DC		5
 #define CLK_PIX		5
 #define CLK_AXIMUX	6
-
-/* Peripheral clocks */
-#define CLK_UART0	3
-#define CLK_UART1	3
-#define CLK_UART2	3
-#define CLK_UART3	3
-#define CLK_UART4	3
-#define CLK_UART5	3
-#define CLK_UART6	3
-#define CLK_UART7	3
-#define CLK_UART8	3
-#define CLK_UART9	3
-#define CLK_UART10	3
-#define CLK_UART11	3
-#define CLK_CAN0	3
-#define CLK_CAN1	3
-#define CLK_I2C0	3
-#define CLK_PWM		3
-#define CLK_I2S		3
-#define CLK_RTC		3
-#define CLK_I2C1	3
-#define CLK_SDIO	3
-#define CLK_I2C2	3
-#define CLK_ADC		3
-#define CLK_NAND	3
-
-#define CLK_WDT		3
+#define CLK_CPU_THROT	7
 
 #endif /* __DT_BINDINGS_LS1C300_CLK_H__ */
