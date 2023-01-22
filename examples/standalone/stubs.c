318b46a90b8d36675b4d8728aa13cf4f094e17cb
Du Huanpeng <dhu@hodcarrier.org>
2023-01-22 21:47:36 +0800

    loongarch: LOONGARCH: examples/standalone/stubs.c
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

--- examples/standalone/stubs.c
+++ examples/standalone/stubs.c
@@ -161,6 +161,29 @@ gd_t *global_data;
 		"	nop\n"				\
 		"	nop\n"				\
 		: : "i"(offsetof(gd_t, jt)), "i"(FO(x)) : "r1", "r2");
+#elif defined(CONFIG_LOONGARCH)
+/*
+ * u0 holds the pointer to the global_data. t0 is call clobbered.
+ */
+#ifdef CONFIG_ARCH_LA64I
+#define EXPORT_FUNC(f, a, x, ...)	\
+	asm volatile (			\
+"	.globl " #x "\n"		\
+#x ":\n"				\
+"	ld	t0, %0(gp)\n"		\
+"	ld	t0, %1(t0)\n"		\
+"	jr	t0\n"			\
+	: : "i"(offsetof(gd_t, jt)), "i"(FO(x)) : "t0");
+#else
+#define EXPORT_FUNC(f, a, x, ...)	\
+	asm volatile (			\
+"	.globl " #x "\n"		\
+#x ":\n"				\
+"	lw	t0, %0(gp)\n"		\
+"	lw	t0, %1(t0)\n"		\
+"	jr	t0\n"			\
+	: : "i"(offsetof(gd_t, jt)), "i"(FO(x)) : "t0");
+#endif
 #elif defined(CONFIG_RISCV)
 /*
  * gp holds the pointer to the global_data. t0 is call clobbered.
