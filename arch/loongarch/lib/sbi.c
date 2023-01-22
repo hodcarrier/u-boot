b24faad7ad7f743023357787a50b366fb96cd4ac
Du Huanpeng <dhu@hodcarrier.org>
2023-01-22 23:44:00 +0800

    loongarch: asm: arch/loongarch/lib/sbi.c
    use syscall to replace ecall, just fool gcc to compile the source.
    
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

--- arch/loongarch/lib/sbi.c
+++ arch/loongarch/lib/sbi.c
@@ -26,7 +26,7 @@ struct sbiret sbi_ecall(int ext, int fid, unsigned long arg0,
 	register uintptr_t a5 asm ("a5") = (uintptr_t)(arg5);
 	register uintptr_t a6 asm ("a6") = (uintptr_t)(fid);
 	register uintptr_t a7 asm ("a7") = (uintptr_t)(ext);
-	asm volatile ("ecall"
+	asm volatile ("syscall 0"
 		      : "+r" (a0), "+r" (a1)
 		      : "r" (a2), "r" (a3), "r" (a4), "r" (a5), "r" (a6), "r" (a7)
 		      : "memory");
