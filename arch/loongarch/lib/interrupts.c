0731a7b9b8d23c8ec4145f68c774559d39600be1
Du Huanpeng <dhu@hodcarrier.org>
2023-08-08 21:37:25 +0800

    loongarch: irq: make empty functions
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

--- arch/loongarch/lib/interrupts.c
+++ arch/loongarch/lib/interrupts.c
@@ -4,23 +4,17 @@
  *
  * (C) Copyright 2000-2009
  * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+ * Copyright (C) 2023 Du Huanpeng <dhu@hodcarrier.org>
  */
 
-#ifndef __IRQ_FUNC_H
-#define __IRQ_FUNC_H
+#include <irq_func.h>
 
-struct pt_regs;
+int interrupt_init(void){return 0;}
+void timer_interrupt(struct pt_regs *regs){}
+void external_interrupt(struct pt_regs *regs){}
+void irq_install_handler(int vec, interrupt_handler_t *handler, void *arg){}
+void irq_free_handler(int vec){}
+void reset_timer(void){}
 
-typedef void (interrupt_handler_t)(void *arg);
-
-int interrupt_init(void);
-void timer_interrupt(struct pt_regs *regs);
-void external_interrupt(struct pt_regs *regs);
-void irq_install_handler(int vec, interrupt_handler_t *handler, void *arg);
-void irq_free_handler(int vec);
-void reset_timer(void);
-
-void enable_interrupts(void);
-int disable_interrupts(void);
-
-#endif
+void enable_interrupts(void){}
+int disable_interrupts(void){return 0;}
