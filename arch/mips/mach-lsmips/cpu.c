8882eb4bb1c59b1c9a9753a185289c5bce5de995
Du Huanpeng <dhu@hodcarrier.org>
2023-10-10 00:32:20 +0800

    lsmips: [hack] CFG_SYS_SDRAM_BASE = 0x80200000
    - gd->ram_size
    - CFG_SYS_SDRAM_BASE
    
    Signed-off-by: Du Huanpeng <dhu@hodcarrier.org>

--- arch/mips/mach-lsmips/cpu.c
+++ arch/mips/mach-lsmips/cpu.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright (C) 2018 Stefan Roese <sr@denx.de>
+ * Copyright (C) 2022-2023 Du Huanpeng <dhu@hodcarrier.org>
  */
 
 #include <common.h>
@@ -14,6 +15,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int dram_init(void)
 {
-	gd->ram_size = get_ram_size((void *)CFG_SYS_SDRAM_BASE, SZ_256M);
+	// FIXME: 配合 CFG_SYS_SDRAM_BASE 设置正确的 ram_size
+	gd->ram_size = get_ram_size((void *)KSEG1, SZ_256M) - CONFIG_TEXT_BASE % SZ_256M;
 	return 0;
 }
--- include/configs/ls1c300.h
+++ include/configs/ls1c300.h
@@ -10,11 +10,14 @@
 #ifndef __CONFIG_LS1C300_H__
 #define __CONFIG_LS1C300_H__
 
-#define CFG_SYS_SDRAM_BASE		0x80000000
+#define CFG_SYS_SDRAM_BASE		0x80200000
 #define CFG_SYS_INIT_SP_OFFSET		0x2000
 
+/* SPL */
+#if defined(CONFIG_SPL_BUILD)
+
 /* Serial SPL */
-#if defined(CONFIG_SPL_BUILD) && defined(CONFIG_SPL_SERIAL)
+#if defined(CONFIG_SPL_SERIAL)
 #define CFG_SYS_NS16550_CLK		66000000
 #define CFG_SYS_NS16550_COM1		0xbfe44000
 #define CFG_SYS_NS16550_COM2		0xbfe48000
@@ -24,11 +27,12 @@
 #define CFG_SYS_NS16550_COM6		0xbfe4c600
 #endif
 
-/* Serial common */
-#define CFG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600 }
-
-/* SPL */
 #define CFG_MALLOC_F_ADDR		0x80100000	/* FIXME: find a proper place */
 #define CFG_SYS_UBOOT_BASE		0xbd000000
+
+#endif
+
+/* Serial common */
+#define CFG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600 }
 
 #endif /* __CONFIG_LS1C300_H__ */
